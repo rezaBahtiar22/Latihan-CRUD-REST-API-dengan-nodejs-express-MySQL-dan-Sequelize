3614740b9645362c1895742398a340056df0b265
Erza_7 <106772717+rezaBahtiar22@users.noreply.github.com>
2024-12-16 22:42:15 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import cors from "cors";
+import userRoute from "./routes/userRoute.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(userRoute);
+
+app.listen(5000, () => console.log('Server up and running'));
